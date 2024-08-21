a8602220226f1f99da668c6b3eb81c3d44d31c76
Jaimin_Patel <jashdekavadiya@gmail.com>
2024-08-21 06:22:51 +0530

    added the coin game

--- lib/games/cricket_screen.dart
+++ lib/games/cricket_screen.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import '../custom_app_bar.dart';
 import 'package:http/http.dart' as http;
-import 'dart:math';
 
 class CricketScreen extends StatelessWidget {
   @override
@@ -41,14 +39,15 @@ class CricketScreen extends StatelessWidget {
   }
 }
 
-
 class RunningMatchesWidget extends StatefulWidget {
   @override
   _RunningMatchesWidgetState createState() => _RunningMatchesWidgetState();
 }
 
 class _RunningMatchesWidgetState extends State<RunningMatchesWidget> {
-  List<Match> _matches = [];
+  List<dynamic> _matches = [];
+  bool _isLoading = true;
+  String _error = '';
 
   @override
   void initState() {
@@ -57,88 +56,117 @@ class _RunningMatchesWidgetState extends State<RunningMatchesWidget> {
   }
 
   Future<void> _fetchMatches() async {
-    final response = await http.get(Uri.parse('https://api.cricapi.com/v1/currentMatches?apikey=f764fc6c-ab15-4698-9c6e-2f315b685b93&offset=0'));
+    final url = Uri.parse('https://api.cricapi.com/v1/currentMatches?apikey=f764fc6c-ab15-4698-9c6e-2f315b685b93&offset=0');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        final List<dynamic> matches = data['data'] ?? [];
 
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-
+        setState(() {
+          _matches = matches;
+          _isLoading = false;
+        });
+      } else {
+        setState(() {
+          _error = 'Failed to load matches: ${response.reasonPhrase}';
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
       setState(() {
-        _matches = data.map((json) => Match.fromJson(json)).toList();
+        _error = 'Failed to load matches: $e';
+        _isLoading = false;
       });
-    } else {
-      throw Exception('Failed to load matches');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _matches.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-        itemCount: _matches.length,
-        itemBuilder: (context, index) {
-          final match = _matches[index];
-          return Card(
-            margin: EdgeInsets.all(12.0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12.0),
-              side: BorderSide(color: Colors.blueAccent, width: 1),
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+    if (_error.isNotEmpty) {
+      return Center(child: Text(_error));
+    }
+
+    return _matches.isEmpty
+        ? Center(child: Text('No matches found.'))
+        : ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemCount: _matches.length,
+      itemBuilder: (context, index) {
+        final match = _matches[index];
+        return Card(
+          margin: EdgeInsets.symmetric(vertical: 8.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            side: BorderSide(color: Colors.blueAccent, width: 1),
+          ),
+          elevation: 4,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${match['name']}',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white54,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  'Teams: ${match['teams'].join(' vs ')}',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  'Venue: ${match['venue']}',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  'Status: ${match['status']}',
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                    color: match['status'] == 'Live' ? Colors.green : Colors.red,
+                  ),
+                ),
+                if (match['score'] != null && match['score'].isNotEmpty) ...[
+                  SizedBox(height: 8.0),
+                  Text(
+                    'Score:',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  for (var score in match['score'])
+                    Text(
+                      '${score['r']} / ${score['w']} in ${score['o']} overs',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.lightGreen,
+                      ),
+                    ),
+                ],
+              ],
             ),
-            elevation: 4,
-            child: ListTile(
-              contentPadding: EdgeInsets.all(16.0),
-              title: Text(
-                '${match.teamA} vs ${match.teamB}',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black87,
-                ),
-              ),
-              subtitle: Text(
-                'Score: ${match.score}',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.grey[600],
-                ),
-              ),
-              trailing: Text(
-                match.status,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                  color: match.status == 'Live' ? Colors.green : Colors.red,
-                ),
-              ),
-            ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
-
-class Match {
-  final String teamA;
-  final String teamB;
-  final String score;
-  final String status;
-
-  Match({required this.teamA, required this.teamB, required this.score, required this.status});
-
-  factory Match.fromJson(Map<String, dynamic> json) {
-    return Match(
-      teamA: json['teamA'],
-      teamB: json['teamB'],
-      score: json['score'],
-      status: json['status'],
-    );
-  }
-}
-
-
-
-
-
-
